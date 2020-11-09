119bc78c506f6182884aefd69e8659e5915dd40e
TheRealChozoElder <nathanbeach@mail.weber.edu>
2020-11-09 05:06:23 -0700

    made server changes

--- server.js
+++ server.js
@@ -1,31 +1,43 @@
-import { connect } from './src/javascripts/config/db/connect'
-import { Movie } from './src/javascripts/models/movie'
+let createError = require('http-errors')
+let express = require('express')
+let path = require('path')
+let cookieParser = require('cookie-parser')
+let logger = require('morgan')
 
+import { connect } from './src/javascripts/config/db/connect'
 connect("mongodb://localhost:27017/topmovies")
 
-Movie.find().exec((err, movies) => {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log(movies)
-    }
+export let app = express()
+
+app.set('views', path.join(__dirname, 'src', 'javascripts', 'views'))
+app.set('view.engine', 'ejs')
+
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+
+//routing
+
+app.use(function (req, res, next) {
+    next(createError(404))
 })
 
-Movie.findOne({ title: "The Godfather" }).exec((err, movie) => {
-    if (err) {
-        console.log(err)
-        return
-    }
-    movie.reviews.push({
-        comment: "This is a test comment",
-        posted_at: new Date()
-    })
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500)
+    res.render(err)
+})
 
-    movie.save(err => {
-        if (err) {
-            console.log('Unable to save')
-        } else {
-            console.log("Movies successfully saved")
-        }
-    })
+let http = require('http')
+let server = http.createServer(app)
+server.listen(process.env.PORT || '8080')
+server.on('error', err => {
+    throw err
+})
+
+server.on('listening', () => {
+    let address = server.address()
+    let bind = typeof address === 'string' ? addresss : address.port
+    console.log("Listening on " + bind)
 })
